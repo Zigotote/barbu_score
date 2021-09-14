add728f2a9cd52fefb950aeb186a45081e9dfb0a
Zigotote <oce88b@gmail.com>
2021-09-14 11:25:31 +0200

    Upd: refacto contract scores

--- lib/pages/contract_scores.dart
+++ lib/pages/contract_scores.dart
@@ -10,6 +10,47 @@ class ContractScores extends GetView<PartyController> {
   /// The contract the player choose
   final ContractsNames contract = Get.arguments;
 
+  List<Widget> _buildFields() {
+    return controller.players.map((player) {
+      if (contract == ContractsNames.Barbu ||
+          contract == ContractsNames.NoLastTrick) {
+        return Row(
+          children: [
+            Text(player.name),
+            Checkbox(value: false, onChanged: (value) => null)
+          ],
+        );
+      } else {
+        int score = 0;
+        return Row(
+          children: [
+            Text(player.name),
+            IconButton(
+              icon: Icon(Icons.remove),
+              onPressed: () => score--,
+            ),
+            Text(score.toString()),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () => score++,
+            ),
+          ],
+        );
+      }
+    }).toList();
+  }
+
+  /// Navigates to the next player or ends the party if no round left
+  void _nextPlayer() {
+    controller.currentPlayer
+        .addContract(contract, {controller.currentPlayer: 0});
+    if (controller.nextPlayer()) {
+      Get.toNamed(Routes.CHOOSE_CONTRACT);
+    } else {
+      Get.toNamed(Routes.HOME);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,16 +60,9 @@ class ContractScores extends GetView<PartyController> {
       body: Column(
         children: [
           Center(child: Text(contract.displayName())),
+          ..._buildFields(),
           OutlinedButton(
-            onPressed: () {
-              controller.currentPlayer
-                  .addContract(contract, {controller.currentPlayer: 0});
-              if (controller.nextPlayer()) {
-                Get.toNamed(Routes.CHOOSE_CONTRACT);
-              } else {
-                Get.toNamed(Routes.HOME);
-              }
-            },
+            onPressed: () => _nextPlayer(),
             child: Text("Joueur suivant"),
           )
         ],
