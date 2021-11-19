af057fed1543c91a7e08f6f49bd487cae0db0093
Zigotote <oce88b@gmail.com>
2021-11-19 12:38:15 +0100

    Upd: improve AbstractContractModel.playerItems getter

--- lib/models/contract_models.dart
+++ lib/models/contract_models.dart
@@ -28,25 +28,19 @@ abstract class AbstractContractModel {
   /// The scores of all the players for this contract
   Map<PlayerController, int> _scores;
 
-  /// The number of item (or rank) of the players. Used to calculate the player scores.
-  Map<PlayerController, int> _playerItems;
-
   AbstractContractModel(this.name) {
     this._scores = {};
-    this._playerItems = {};
   }
 
   Map<PlayerController, int> get scores => _scores;
 
-  Map<PlayerController, int> get playerItems => _playerItems;
+  /// The number of item (or rank) of the players, calculated from _scores. Used to modify the contract.
+  Map<PlayerController, int> get playerItems;
 
   /// Sets the score of each player from a Map wich links all players with the number of tricks/cards they won.
   /// The players not present in the list have a score of 0.
   /// Returns true if the score has been set correctly, false if the trickByPlayer Map is not correctly filled
-  bool setScores(Map<PlayerController, int> playerItems) {
-    this._playerItems = playerItems;
-    return false;
-  }
+  bool setScores(Map<PlayerController, int> playerItems);
 }
 
 /// An abstract class to fill the scores for a contract which has only one looser
@@ -57,11 +51,17 @@ abstract class AbstractOneLooserContractModel extends AbstractContractModel {
   AbstractOneLooserContractModel(ContractsNames name, this._points)
       : super(name);
 
+  @override
+  Map<PlayerController, int> get playerItems => Map.fromIterable(
+        scores.entries,
+        key: (playerScore) => playerScore.key,
+        value: (playerScore) => playerScore.value == _points ? 1 : 0,
+      );
+
   /// Sets the score of the player in the Map at the value of this.points. Other players have a score of 0.
   /// Returns true if the score has been set correctly, false otherwise (less or more than 1 player in the Map)
   @override
   bool setScores(Map<PlayerController, int> trickByPlayer) {
-    super.setScores(trickByPlayer);
     if (trickByPlayer.entries.length != 1) {
       return false;
     }
@@ -92,8 +92,15 @@ abstract class AbstractMultipleLooserContractModel
   int get expectedItems => _expectedItems;
 
   @override
+  Map<PlayerController, int> get playerItems => Map.fromIterable(
+        scores.entries,
+        key: (playerScore) => playerScore.key,
+        value: (playerScore) =>
+            ((playerScore.value as int) ~/ this._pointsByItem).abs(),
+      );
+
+  @override
   bool setScores(Map<PlayerController, int> itemsByPlayer) {
-    super.setScores(itemsByPlayer);
     final int declaredItems = itemsByPlayer.values
         .fold(0, (previousValue, element) => previousValue + element);
     if (declaredItems != this._expectedItems) {
@@ -159,7 +166,10 @@ class TrumpsContractModel extends AbstractContractModel {
   TrumpsContractModel() : super(ContractsNames.Trumps);
 
   @override
-  bool setScores(Map<PlayerController, int> playerScores) {
+  Map<PlayerController, int> get playerItems => {};
+
+  @override
+  bool setScores(Map<PlayerController, int> playerScores, {int a}) {
     this._scores = playerScores;
     return true;
   }
@@ -167,12 +177,25 @@ class TrumpsContractModel extends AbstractContractModel {
 
 /// A domino contract scores
 class DominoContractModel extends AbstractContractModel {
+  /// The scores the player can have (it is a symmetric array)
+  final List<int> _rankScores = [40, 20, 10];
+
   DominoContractModel() : super(ContractsNames.Domino);
+
+  @override
+  Map<PlayerController, int> get playerItems {
+    List<MapEntry<PlayerController, int>> sortedList = scores.entries.toList();
+    sortedList.sort((a, b) => a.value.compareTo(b.value));
+    return Map.fromIterable(
+      sortedList,
+      key: (playerScore) => playerScore.key,
+      value: (playerScore) => sortedList.indexOf(playerScore),
+    );
+  }
 
   /// Sets the score of each player from a Map wich links each player with its rank
   @override
   bool setScores(Map<PlayerController, int> rankOfPlayer) {
-    super.setScores(rankOfPlayer);
     if (rankOfPlayer.length != Get.find<PartyController>().nbPlayers) {
       return false;
     }
@@ -182,8 +205,6 @@ class DominoContractModel extends AbstractContractModel {
       (element1, element2) => element1.value - element2.value,
     );
 
-    /// The scores the player can have (it is a symmetric array)
-    List<int> scores = [40, 20, 10];
     double middleIndex = orderedPlayers.length / 2;
     orderedPlayers.forEach((player) {
       int score = 0;
@@ -193,9 +214,9 @@ class DominoContractModel extends AbstractContractModel {
       if (orderedPlayers.length % 2 == 0 || middleIndex - 0.5 != playerIndex) {
         /// The firsts players have a negative score, other have a positive score
         if (playerIndex < middleIndex) {
-          score -= scores[playerIndex];
+          score -= this._rankScores[playerIndex];
         } else {
-          score = scores[orderedPlayers.length - playerIndex - 1];
+          score = this._rankScores[orderedPlayers.length - playerIndex - 1];
         }
       }
       this._scores[player.key] = score;
