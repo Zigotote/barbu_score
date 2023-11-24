e9a50314f53c0ecd6fff5d3eec5f9902b6fdd52c
BERTHE Océane <oceane.berthe@externe.maif.fr>
2023-11-24 09:44:13 +0100

    refactor: remove PlayerIconProperties class to follow dart rules

--- lib/commons/utils/player_icon_properties.dart
+++ lib/commons/utils/player_icon_properties.dart
@@ -3,41 +3,36 @@ import 'package:sprintf/sprintf.dart';
 
 import 'globals.dart';
 
-/// A class to get the properties available to configure players icons
-class PlayerIconProperties {
-  static final _isDecember = DateTime.now().month == 12;
+final _isDecember = DateTime.now().month == 12;
 
-  PlayerIconProperties._();
-
-  /// Returns the list of player images available
-  static List<String> get playerImages {
-    const nbImages = 13;
-    const christmasImageSuffix = "_christmas";
-    return List.generate(nbImages, (index) {
-      if (_isDecember) {
-        if (index == 0) {
-          return sprintf(kPlayerImageFolder, [christmasImageSuffix]);
-        } else {
-          return sprintf(kPlayerImageFolder, [index]);
-        }
-      } else {
-        if (index == nbImages - 1) {
-          return sprintf(kPlayerImageFolder, [christmasImageSuffix]);
-        } else {
-          return sprintf(kPlayerImageFolder, [index + 1]);
-        }
-      }
-    });
-  }
-
-  /// Returns the list of colors available to customize the player
-  static List<PlayerColors> get playerColors {
+/// Returns the list of player images available
+List<String> get playerImages {
+  const nbImages = 13;
+  const christmasImageSuffix = "_christmas";
+  return List.generate(nbImages, (index) {
     if (_isDecember) {
-      return [
-        PlayerColors.red,
-        ...PlayerColors.values.where((color) => color != PlayerColors.red)
-      ];
+      if (index == 0) {
+        return sprintf(kPlayerImageFolder, [christmasImageSuffix]);
+      } else {
+        return sprintf(kPlayerImageFolder, [index]);
+      }
+    } else {
+      if (index == nbImages - 1) {
+        return sprintf(kPlayerImageFolder, [christmasImageSuffix]);
+      } else {
+        return sprintf(kPlayerImageFolder, [index + 1]);
+      }
     }
-    return PlayerColors.values;
+  });
+}
+
+/// Returns the list of colors available to customize the player
+List<PlayerColors> get playerColors {
+  if (_isDecember) {
+    return [
+      PlayerColors.red,
+      ...PlayerColors.values.where((color) => color != PlayerColors.red)
+    ];
   }
+  return PlayerColors.values;
 }
