472126202c1e79ee4f48b0c165b933d45cc13a4b
BERTHE Océane <oceane.berthe@externe.maif.fr>
2023-12-21 15:41:33 +0100

    fix: close previous snackbar before opening a new one

--- lib/commons/utils/snackbar.dart
+++ lib/commons/utils/snackbar.dart
@@ -14,28 +14,29 @@ class SnackBarUtils {
       {required BuildContext context,
       required String title,
       required String text}) {
-    if (!_instance._isSnackBarOpen) {
-      _instance._isSnackBarOpen = true;
-      final TextTheme textTheme = Theme.of(context).textTheme;
-      ScaffoldMessenger.of(context)
-          .showSnackBar(
-            SnackBar(
-              content: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(title, style: textTheme.titleMedium),
-                  const SizedBox(height: 4),
-                  Text(text, style: textTheme.bodyLarge),
-                ],
-              ),
-              behavior: SnackBarBehavior.floating,
-              margin: const EdgeInsets.only(right: 8, left: 8, bottom: 8),
-              dismissDirection: DismissDirection.horizontal,
-            ),
-          )
-          .closed
-          .then((_) => _instance._isSnackBarOpen = false);
+    if (_instance._isSnackBarOpen) {
+      ScaffoldMessenger.of(context).removeCurrentSnackBar();
     }
+    _instance._isSnackBarOpen = true;
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    ScaffoldMessenger.of(context)
+        .showSnackBar(
+          SnackBar(
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(title, style: textTheme.titleMedium),
+                const SizedBox(height: 4),
+                Text(text, style: textTheme.bodyLarge),
+              ],
+            ),
+            behavior: SnackBarBehavior.floating,
+            margin: const EdgeInsets.only(right: 8, left: 8, bottom: 8),
+            dismissDirection: DismissDirection.horizontal,
+          ),
+        )
+        .closed
+        .then((_) => _instance._isSnackBarOpen = false);
   }
 
   void closeSnackBar(BuildContext context) {
