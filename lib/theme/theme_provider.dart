478bd8ebf614d520638d073749e56240c26f53b4
BERTHE Océane <oceane.berthe@externe.maif.fr>
2024-08-19 18:52:18 +0200

    fix: use storage_provider in theme_provider

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -2,18 +2,24 @@ import 'package:barbu_score/commons/notifiers/storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../commons/utils/storage.dart';
-
 final isDarkThemeProvider = StateNotifierProvider<_IsDarkThemeNotifier, bool?>(
-  (ref) => _IsDarkThemeNotifier(ref.watch(storageProvider).getIsDarkTheme()),
+  (ref) {
+    final storage = ref.read(storageProvider);
+    return _IsDarkThemeNotifier(
+      storage.getIsDarkTheme(),
+      saveChanges: storage.saveIsDarkTheme,
+    );
+  },
 );
 
 class _IsDarkThemeNotifier extends StateNotifier<bool?> {
-  _IsDarkThemeNotifier(super._state);
+  final Function(bool) saveChanges;
+
+  _IsDarkThemeNotifier(super._state, {required this.saveChanges});
 
   void changeTheme(bool isDark) {
     state = isDark;
-    MyStorage.saveIsDarkTheme(isDark);
+    saveChanges(isDark);
   }
 
   /// Returns true if dark theme is set or system theme is dark. False otherwise
