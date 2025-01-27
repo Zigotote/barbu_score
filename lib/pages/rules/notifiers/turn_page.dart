9f245aeb4cd4717d22c82d6296cbf0291cf3a51b
BERTHE Océane <oceane.berthe@externe.maif.fr>
2025-01-27 13:33:59 +0100

    chore: use my version of turn page transition

--- lib/pages/rules/notifiers/turn_page.dart
+++ lib/pages/rules/notifiers/turn_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:turn_page_transition/turn_page_transition.dart';
 
-final turnPageProvider = Provider.autoDispose((_) => TurnPageController());
+final turnPageProvider = Provider.autoDispose(
+  (_) => TurnPageController(startCorner: TurnCorner.bottomRight),
+);
