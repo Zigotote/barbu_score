6499ccb98981b17916f142971617cbc9496bc78d
Zigotote <oce88b@gmail.com>
2025-10-31 15:44:54 +0100

    :bug: fix system theme not working

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,16 +1,14 @@
 import 'package:barbu_score/commons/providers/storage.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final isDarkThemeProvider =
-    NotifierProvider<_IsDarkThemeNotifier, bool>(_IsDarkThemeNotifier.new);
+final isDarkThemeProvider = NotifierProvider<_IsDarkThemeNotifier, bool?>(
+  _IsDarkThemeNotifier.new,
+);
 
-class _IsDarkThemeNotifier extends Notifier<bool> {
+class _IsDarkThemeNotifier extends Notifier<bool?> {
   @override
-  bool build() {
-    return ref.read(storageProvider).getIsDarkTheme() ??
-        WidgetsBinding.instance.platformDispatcher.platformBrightness ==
-            Brightness.dark;
+  bool? build() {
+    return ref.read(storageProvider).getIsDarkTheme();
   }
 
   void changeTheme(bool isDark) {
