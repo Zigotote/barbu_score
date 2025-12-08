f35688f1b07daa8cb91d44759ac518f8ffedf4cc
Océane <oce88b@gmail.com>
2025-12-08 10:09:44 +0100

    bookmark: 3.9.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "zigotote.barbu_score"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 25
-        versionName = "3.8.1"
+        versionCode = 26
+        versionName = "3.9.0"
     }
 
     signingConfigs {
