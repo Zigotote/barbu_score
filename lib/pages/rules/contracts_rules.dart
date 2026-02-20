1a327aa951140a49979a677934183ea7e373af04
Océane <oce88b@gmail.com>
2026-02-20 09:36:09 +0100

    wip: design Thomas

--- lib/pages/rules/contracts_rules.dart
+++ lib/pages/rules/contracts_rules.dart
@@ -4,14 +4,11 @@ import 'package:barbu_score/theme/my_themes.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../commons/models/contract_info.dart';
 import '../../commons/providers/log.dart';
 import '../../commons/providers/storage.dart';
-import '../settings/widgets/change_contract_activation.dart';
-import '../settings/widgets/my_switch.dart';
-import '../settings/widgets/number_input.dart';
-import '../settings/widgets/setting_question.dart';
 import 'widgets/rules_page.dart';
 import 'widgets/settings_card.dart';
 
@@ -77,10 +74,10 @@ class TestAnimatedWidget extends ConsumerStatefulWidget {
 class _TestAnimatedWidgetState extends ConsumerState<TestAnimatedWidget>
     with TickerProviderStateMixin {
   bool showSettings = false;
-  bool isExpanded = false;
+  bool isExpanded = true;
   late final AnimationController _controller;
-  late final Animation<double> _tweenAnimation;
-  final Tween<double> _expandTween = Tween(begin: 0, end: 1);
+
+  late final Animation<double> _animation;
 
   @override
   void initState() {
@@ -88,10 +85,15 @@ class _TestAnimatedWidgetState extends ConsumerState<TestAnimatedWidget>
       vsync: this,
       duration: const Duration(milliseconds: 150),
     );
-    _tweenAnimation = CurvedAnimation(
+    _animation = CurvedAnimation(
       parent: _controller,
       curve: Curves.fastOutSlowIn,
     );
+    if (isExpanded) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
     super.initState();
   }
 
@@ -120,189 +122,166 @@ class _TestAnimatedWidgetState extends ConsumerState<TestAnimatedWidget>
         );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final settings = ref.watch(storageProvider).getSettings(widget.contract);
-    return Column(
+  BoxDecoration _cardDecoration(Color color) {
+    return BoxDecoration(
+      borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
+      color: color,
+    );
+  }
+
+  Row _buildHeader(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        GestureDetector(
-          onTap: () {
-            final newIsExpanded = !isExpanded;
-            setState(() => isExpanded = newIsExpanded);
-            if (newIsExpanded) {
-              _controller.forward();
-            } else {
-              _controller.reverse();
-            }
-          },
-          child: Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.vertical(
-                    top: Radius.circular(20),
-                  ),
-                  border: BoxBorder.symmetric(
-                    vertical: BorderSide(
-                      color: Theme.of(context).colorScheme.convertMyColor(
-                        widget.contract.color,
-                        isBackgroundColor: true,
-                      ),
-                    ),
-                  ),
-                  color: Theme.of(context).colorScheme.convertMyColor(
-                    widget.contract.color,
-                    isBackgroundColor: true,
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  spacing: 8,
-                  children: [
-                    Text(
-                      context.l10n.contractName(widget.contract),
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        isExpanded
-                            ? Icons.keyboard_arrow_down_outlined
-                            : Icons.keyboard_arrow_up_outlined,
-                      ),
-                      tooltip:
-                          "${context.l10n.settings} ${context.l10n.contractName(widget.contract)}",
-                      style: IconButtonTheme.of(context).style?.copyWith(
-                        backgroundColor: WidgetStatePropertyAll(
-                          Colors.transparent,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        SizeTransition(
-          sizeFactor: _expandTween.animate(_tweenAnimation),
+        _buildCardName(context),
+        Expanded(
           child: Container(
+            height: 16,
             decoration: BoxDecoration(
-              border: BoxBorder.symmetric(
-                vertical: BorderSide(
-                  color: Theme.of(context).colorScheme.convertMyColor(
-                    widget.contract.color,
-                    isBackgroundColor: true,
-                  ),
-                  width: 2,
-                ),
+              color: Theme.of(context).colorScheme.convertMyColor(
+                widget.contract.color,
+                isBackgroundColor: true,
               ),
-            ),
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: showSettings
-                  ? Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ChangeContractActivation(widget.contract, settings),
-                        SettingQuestion(
-                          label: context.l10n.contractPoints,
-                          onTap: null,
-                          input: NumberInput(
-                            value:
-                                (settings as ContractWithPointsSettings).points,
-                            onChanged: (value) {
-                              if (value != settings.points) {
-                                settings.points = value;
-                                saveNewSettings(ref, widget.contract, settings);
-                              }
-                            },
-                            focusNode: null,
-                          ),
-                        ),
-                        if (settings.canInvertScore)
-                          SettingQuestion(
-                            tooltip: context.l10n.invertScoreNegativeDetails,
-                            label: context.l10n.invertScore,
-                            onTap: () {
-                              settings.invertScore = !settings.invertScore;
-                              saveNewSettings(ref, widget.contract, settings);
-                            },
-                            input: MySwitch(
-                              isActive: settings.invertScore,
-                              onChanged: (value) {
-                                settings.invertScore = value;
-                                saveNewSettings(ref, widget.contract, settings);
-                              },
-                            ),
-                          ),
-                        SizedBox(height: 8),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            TextButton(
-                              onPressed: () =>
-                                  setState(() => showSettings = !showSettings),
-                              child: Text("Annuler"),
-                            ),
-                            ElevatedButton(
-                              onPressed: () =>
-                                  setState(() => showSettings = !showSettings),
-                              child: Text("Appliquer"),
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  : Column(
-                      key: Key(widget.contract.name),
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      spacing: 8,
-                      children: [
-                        Text(
-                          context.l10n.contractRules(
-                            widget.contract,
-                            ref.read(storageProvider),
-                          ),
-                        ),
-                        if (!settings.isActive)
-                          Text(
-                            context.l10n.deactivatedForGame,
-                            style: const TextStyle(fontStyle: FontStyle.italic),
-                          ),
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: TextButton.icon(
-                            onPressed: () =>
-                                setState(() => showSettings = !showSettings),
-                            icon: Icon(Icons.settings),
-                            label: Text(
-                              "Modifier",
-                              style: TextStyle(fontSize: 16),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
             ),
           ),
         ),
         Container(
           decoration: BoxDecoration(
-            border: BoxBorder.symmetric(
-              vertical: BorderSide(
-                color: Theme.of(context).colorScheme.convertMyColor(
-                  widget.contract.color,
-                  isBackgroundColor: true,
-                ),
-                width: 2,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(15),
+              topRight: Radius.circular(15),
+            ),
+            border: Border.all(
+              width: 3,
+              color: Theme.of(context).colorScheme.convertMyColor(
+                widget.contract.color,
+                isBackgroundColor: true,
               ),
             ),
           ),
-          height: 8,
+          child: IconButton(
+            onPressed: () => context.push(widget.contract.settingsRoute),
+            icon: Icon(Icons.settings),
+            visualDensity: VisualDensity.compact,
+          ),
+        ),
+        Container(
+          height: 16,
+          width: 15,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(15),
+            ),
+            color: Theme.of(context).colorScheme.convertMyColor(
+              widget.contract.color,
+              isBackgroundColor: true,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  GestureDetector _buildCardName(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final newIsExpanded = !isExpanded;
+
+        setState(() => isExpanded = newIsExpanded);
+        if (newIsExpanded) {
+          _controller.forward();
+        } else {
+          _controller.reverse();
+        }
+      },
+      child: Container(
+        padding: EdgeInsets.fromLTRB(12, 0, 0, 8),
+        decoration: _cardDecoration(
+          Theme.of(context).colorScheme.convertMyColor(
+            widget.contract.color,
+            isBackgroundColor: true,
+          ),
+        ),
+        child: Row(
+          children: [
+            Semantics(
+              header: true,
+              child: Text(context.l10n.contractName(widget.contract)),
+            ),
+            IconButton(
+              onPressed: () => setState(() => isExpanded = !isExpanded),
+              icon: Icon(
+                isExpanded
+                    ? Icons.keyboard_arrow_down_outlined
+                    : Icons.keyboard_arrow_up_outlined,
+              ),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.transparent),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRules(AbstractContractSettings settings) {
+    return Column(
+      key: Key(widget.contract.name),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 8,
+      children: [
+        Text(
+          context.l10n.contractRules(
+            widget.contract,
+            ref.read(storageProvider),
+          ),
+        ),
+        if (!settings.isActive)
+          Text(
+            context.l10n.deactivatedForGame,
+            style: const TextStyle(fontStyle: FontStyle.italic),
+          ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final settings = ref.watch(storageProvider).getSettings(widget.contract);
+    return Column(
+      children: [
+        Stack(
+          alignment: Alignment.topRight,
+          children: [
+            _buildHeader(context),
+            if (widget.previousContract != null)
+              Container(
+                width: 3,
+                color: Theme.of(context).colorScheme.convertMyColor(
+                  widget.previousContract!.color,
+                  isBackgroundColor: true,
+                ),
+                height: 40,
+              ),
+          ],
+        ),
+        Container(
+          color: Theme.of(context).colorScheme.convertMyColor(
+            widget.contract.color,
+            isBackgroundColor: true,
+          ),
+          child: Container(
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.symmetric(horizontal: 3),
+            decoration: _cardDecoration(
+              Theme.of(context).scaffoldBackgroundColor,
+            ),
+            child: SizeTransition(
+              sizeFactor: _animation,
+              child: _buildRules(settings),
+            ),
+          ),
         ),
       ],
     );
