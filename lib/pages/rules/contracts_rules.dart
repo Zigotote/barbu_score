c086ddccf99ee73147631ed04c53bdde3871c019
Océane <oce88b@gmail.com>
2026-03-09 16:55:33 +0100

    refacto: create contract_divider_widget

--- lib/pages/rules/contracts_rules.dart
+++ lib/pages/rules/contracts_rules.dart
@@ -1,13 +1,11 @@
-import 'package:barbu_score/commons/models/contract_settings_models.dart';
 import 'package:barbu_score/commons/utils/l10n_extensions.dart';
+import 'package:barbu_score/pages/rules/widgets/contract_divider_widget.dart';
 import 'package:barbu_score/theme/my_themes.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../commons/models/contract_info.dart';
-import '../../commons/providers/log.dart';
 import '../../commons/providers/storage.dart';
 import 'widgets/rules_page.dart';
 import 'widgets/settings_card.dart';
@@ -41,249 +39,21 @@ class ContractsRules extends ConsumerWidget {
                 }
                 return true;
               })
-              .mapIndexed((index, contract) {
-                return TestAnimatedWidget(
+              .mapIndexed(
+                (index, contract) => ContractDividerWidget(
                   contract: contract,
-                  previousContract: index - 1 >= 0
-                      ? ContractsInfo.values[index - 1]
+                  previousContractDividerColor: index - 1 >= 0
+                      ? Theme.of(context).colorScheme.convertMyColor(
+                          ContractsInfo.values[index - 1].color,
+                          isBackgroundColor: true,
+                        )
                       : null,
-                );
-              }),
+                ),
+              ),
           SizedBox(height: 16),
           const SettingsCard(),
         ],
       ),
-    );
-  }
-}
-
-class TestAnimatedWidget extends ConsumerStatefulWidget {
-  final ContractsInfo contract;
-  final ContractsInfo? previousContract;
-
-  const TestAnimatedWidget({
-    super.key,
-    required this.contract,
-    this.previousContract,
-  });
-
-  @override
-  ConsumerState<TestAnimatedWidget> createState() => _TestAnimatedWidgetState();
-}
-
-class _TestAnimatedWidgetState extends ConsumerState<TestAnimatedWidget>
-    with TickerProviderStateMixin {
-  bool showSettings = false;
-  bool isExpanded = true;
-  late final AnimationController _controller;
-
-  late final Animation<double> _animation;
-
-  @override
-  void initState() {
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 150),
-    );
-    _animation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.fastOutSlowIn,
-    );
-    if (isExpanded) {
-      _controller.forward();
-    } else {
-      _controller.reverse();
-    }
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  /// Saves new settings
-  void saveNewSettings(
-    WidgetRef ref,
-    ContractsInfo contract,
-    AbstractContractSettings settings,
-  ) {
-    ref.read(storageProvider).saveSettings(contract, settings);
-    ref.invalidate(storageProvider);
-    ref
-        .read(logProvider)
-        .info("MySettings: save ${contract.name} settings $settings");
-    ref
-        .read(logProvider)
-        .sendAnalyticEvent(
-          "modify_settings",
-          parameters: {"contract": contract.name},
-        );
-  }
-
-  BoxDecoration _cardDecoration(Color color) {
-    return BoxDecoration(
-      borderRadius: const BorderRadius.vertical(top: Radius.circular(15)),
-      color: color,
-    );
-  }
-
-  Row _buildHeader(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        _buildCardName(context),
-        Expanded(
-          child: Container(
-            height: 16,
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.convertMyColor(
-                widget.contract.color,
-                isBackgroundColor: true,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(15),
-              topRight: Radius.circular(15),
-            ),
-            border: Border.all(
-              width: 3,
-              color: Theme.of(context).colorScheme.convertMyColor(
-                widget.contract.color,
-                isBackgroundColor: true,
-              ),
-            ),
-          ),
-          child: IconButton(
-            onPressed: () => context.push(widget.contract.settingsRoute),
-            icon: Icon(Icons.settings),
-            visualDensity: VisualDensity.compact,
-          ),
-        ),
-        Container(
-          height: 16,
-          width: 15,
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              topRight: Radius.circular(15),
-            ),
-            color: Theme.of(context).colorScheme.convertMyColor(
-              widget.contract.color,
-              isBackgroundColor: true,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  GestureDetector _buildCardName(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        final newIsExpanded = !isExpanded;
-
-        setState(() => isExpanded = newIsExpanded);
-        if (newIsExpanded) {
-          _controller.forward();
-        } else {
-          _controller.reverse();
-        }
-      },
-      child: Container(
-        padding: EdgeInsets.fromLTRB(12, 0, 0, 8),
-        decoration: _cardDecoration(
-          Theme.of(context).colorScheme.convertMyColor(
-            widget.contract.color,
-            isBackgroundColor: true,
-          ),
-        ),
-        child: Row(
-          children: [
-            Semantics(
-              header: true,
-              child: Text(context.l10n.contractName(widget.contract)),
-            ),
-            IconButton(
-              onPressed: () => setState(() => isExpanded = !isExpanded),
-              icon: Icon(
-                isExpanded
-                    ? Icons.keyboard_arrow_down_outlined
-                    : Icons.keyboard_arrow_up_outlined,
-              ),
-              style: ButtonStyle(
-                backgroundColor: WidgetStatePropertyAll(Colors.transparent),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildRules(AbstractContractSettings settings) {
-    return Column(
-      key: Key(widget.contract.name),
-      crossAxisAlignment: CrossAxisAlignment.start,
-      spacing: 8,
-      children: [
-        Text(
-          context.l10n.contractRules(
-            widget.contract,
-            ref.read(storageProvider),
-          ),
-        ),
-        if (!settings.isActive)
-          Text(
-            context.l10n.deactivatedForGame,
-            style: const TextStyle(fontStyle: FontStyle.italic),
-          ),
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final settings = ref.watch(storageProvider).getSettings(widget.contract);
-    return Column(
-      children: [
-        Stack(
-          alignment: Alignment.topRight,
-          children: [
-            _buildHeader(context),
-            if (widget.previousContract != null)
-              Container(
-                width: 3,
-                color: Theme.of(context).colorScheme.convertMyColor(
-                  widget.previousContract!.color,
-                  isBackgroundColor: true,
-                ),
-                height: 40,
-              ),
-          ],
-        ),
-        Container(
-          color: Theme.of(context).colorScheme.convertMyColor(
-            widget.contract.color,
-            isBackgroundColor: true,
-          ),
-          child: Container(
-            padding: EdgeInsets.all(12),
-            margin: EdgeInsets.symmetric(horizontal: 3),
-            decoration: _cardDecoration(
-              Theme.of(context).scaffoldBackgroundColor,
-            ),
-            child: SizeTransition(
-              sizeFactor: _animation,
-              child: _buildRules(settings),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ lib/pages/rules/widgets/contract_divider_widget.dart
@@ -0,0 +1,213 @@
+import 'package:barbu_score/commons/utils/l10n_extensions.dart';
+import 'package:barbu_score/theme/my_themes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../commons/models/contract_info.dart';
+import '../../../commons/models/contract_settings_models.dart';
+import '../../../commons/providers/storage.dart';
+
+class ContractDividerWidget extends ConsumerStatefulWidget {
+  final ContractsInfo contract;
+  final Color? previousContractDividerColor;
+
+  const ContractDividerWidget({
+    super.key,
+    required this.contract,
+    this.previousContractDividerColor,
+  });
+
+  @override
+  ConsumerState<ContractDividerWidget> createState() =>
+      _ContractDividerWidgetState();
+}
+
+class _ContractDividerWidgetState extends ConsumerState<ContractDividerWidget>
+    with TickerProviderStateMixin {
+  static const Radius borderRadius = Radius.circular(15);
+
+  static const double borderWidth = 3;
+
+  Color get borderColor => Theme.of(
+    context,
+  ).colorScheme.convertMyColor(widget.contract.color, isBackgroundColor: true);
+
+  /// Indicates if the widget is in rules view, or settings view
+  bool showSettings = false;
+
+  /// Indicates if the content of the widget is displayed
+  late bool _isExpanded;
+
+  /// The controller for the open/close animation
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    _isExpanded = ref
+        .read(storageProvider)
+        .getSettings(widget.contract)
+        .isActive;
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.fastOutSlowIn,
+    );
+    if (_isExpanded) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleExpansion() {
+    final newIsExpanded = !_isExpanded;
+
+    setState(() => _isExpanded = newIsExpanded);
+    if (newIsExpanded) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+  }
+
+  /// Builds the header of the divider, which contains the contract name, the arrow to expand/collapse the divider and the button to switch between settings and rules display
+  Row _buildHeader() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        _buildDividerName(),
+        Expanded(
+          child: Container(
+            height: 16,
+            decoration: BoxDecoration(color: borderColor),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              topLeft: borderRadius,
+              topRight: borderRadius,
+            ),
+            border: Border.all(width: borderWidth, color: borderColor),
+          ),
+          child: IconButton(
+            onPressed: () => context.push(widget.contract.settingsRoute),
+            icon: Icon(Icons.settings),
+            visualDensity: VisualDensity.compact,
+          ),
+        ),
+        Container(
+          height: 16,
+          width: 16,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(topRight: borderRadius),
+            color: borderColor,
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// Builds the content of divider header, with the name of the contract and the button to toggle expansion
+  GestureDetector _buildDividerName() {
+    return GestureDetector(
+      onTap: _toggleExpansion,
+      child: Container(
+        padding: EdgeInsets.fromLTRB(12, 0, 0, 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(top: borderRadius),
+          color: borderColor,
+        ),
+        child: Row(
+          children: [
+            Semantics(
+              header: true,
+              child: Text(context.l10n.contractName(widget.contract)),
+            ),
+            IconButton(
+              onPressed: _toggleExpansion,
+              icon: Icon(
+                _isExpanded
+                    ? Icons.keyboard_arrow_down_outlined
+                    : Icons.keyboard_arrow_up_outlined,
+              ),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.transparent),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Builds the rules widget
+  Widget _buildRules(AbstractContractSettings settings) {
+    return Column(
+      key: Key(widget.contract.name),
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 8,
+      children: [
+        if (!settings.isActive)
+          Text(
+            context.l10n.deactivatedForGame,
+            style: const TextStyle(fontStyle: FontStyle.italic),
+          ),
+        Text(
+          context.l10n.contractRules(
+            widget.contract,
+            ref.read(storageProvider),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final settings = ref.watch(storageProvider).getSettings(widget.contract);
+    return Column(
+      children: [
+        Stack(
+          alignment: Alignment.topRight,
+          children: [
+            _buildHeader(),
+            if (widget.previousContractDividerColor != null)
+              Container(
+                width: borderWidth,
+                color: widget.previousContractDividerColor,
+                height: MediaQuery.textScalerOf(context).scale(40),
+              ),
+          ],
+        ),
+        Container(
+          color: borderColor,
+          child: Container(
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.symmetric(horizontal: borderWidth),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.vertical(top: borderRadius),
+              color: Theme.of(context).scaffoldBackgroundColor,
+            ),
+            child: SizeTransition(
+              sizeFactor: _animation,
+              child: _buildRules(settings),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
