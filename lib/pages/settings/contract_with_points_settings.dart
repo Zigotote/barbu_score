cf30a43c689af3fed931d82afb4513df3ec72d57
Océane <oce88b@gmail.com>
2026-03-10 17:45:03 +0100

    wip: add settings view

--- lib/pages/settings/contract_with_points_settings.dart
+++ lib/pages/settings/contract_with_points_settings.dart
@@ -5,10 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../commons/models/contract_info.dart';
 import '../../commons/models/contract_settings_models.dart';
 import '../../commons/providers/storage.dart';
-import '../../commons/widgets/my_appbar.dart';
-import '../../commons/widgets/my_default_page.dart';
 import 'utils/change_settings.dart';
-import 'widgets/change_contract_activation.dart';
 import 'widgets/my_switch.dart';
 import 'widgets/number_input.dart';
 import 'widgets/setting_question.dart';
@@ -27,53 +24,41 @@ class ContractWithPointsSettingsPage extends ConsumerWidget
         ref.read(storageProvider).getSettings(contract).copyWith()
             as ContractWithPointsSettings;
     final numberFocusNode = FocusNode();
-    return MyDefaultPage(
-      appBar: MyAppBar(
-        Column(
-          children: [
-            Text(context.l10n.settings),
-            Text(context.l10n.contractName(contract)),
-          ],
-        ),
-        context: context,
-      ),
-      content: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        spacing: 16,
-        children: [
-          ChangeContractActivation(contract, settings),
-          SettingQuestion(
-            label: context.l10n.contractPoints,
-            onTap: numberFocusNode.requestFocus,
-            input: NumberInput(
-              value: settings.points,
-              onChanged: (value) {
-                if (value != settings.points) {
-                  settings.points = value;
-                  saveNewSettings(ref, contract, settings);
-                }
-              },
-              focusNode: numberFocusNode,
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 8,
+      children: [
+        SettingQuestion(
+          label: context.l10n.contractPoints,
+          onTap: numberFocusNode.requestFocus,
+          input: NumberInput(
+            value: settings.points,
+            onChanged: (value) {
+              if (value != settings.points) {
+                settings.points = value;
+                saveNewSettings(ref, contract, settings);
+              }
+            },
+            focusNode: numberFocusNode,
           ),
-          if (settings.canInvertScore)
-            SettingQuestion(
-              tooltip: context.l10n.detailedInvertScoreRules(settings.points),
-              label: context.l10n.invertScore,
-              onTap: () {
-                settings.invertScore = !settings.invertScore;
+        ),
+        if (settings.canInvertScore)
+          SettingQuestion(
+            tooltip: context.l10n.detailedInvertScoreRules(settings.points),
+            label: context.l10n.invertScore,
+            onTap: () {
+              settings.invertScore = !settings.invertScore;
+              saveNewSettings(ref, contract, settings);
+            },
+            input: MySwitch(
+              isActive: settings.invertScore,
+              onChanged: (value) {
+                settings.invertScore = value;
                 saveNewSettings(ref, contract, settings);
               },
-              input: MySwitch(
-                isActive: settings.invertScore,
-                onChanged: (value) {
-                  settings.invertScore = value;
-                  saveNewSettings(ref, contract, settings);
-                },
-              ),
             ),
-        ],
-      ),
+          ),
+      ],
     );
   }
 }
