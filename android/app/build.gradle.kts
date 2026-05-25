d07d2d23089e7e2d20e5d44c1d2f975d979b4d30
Océane <oce88b@gmail.com>
2026-05-25 20:17:59 +0200

    :bookmark: 3.11.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "zigotote.barbu_score"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 28
-        versionName = "3.10.1"
+        versionCode = 29
+        versionName = "3.11.0"
     }
 
     signingConfigs {
