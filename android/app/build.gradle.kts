41132121408ea8afd9f6e9940fb2ffa0e0516a5e
Océane <oce88b@gmail.com>
2026-08-22 09:42:44 +0200

    :arrow_up: upgrade gradle from 8 to 9

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "zigotote.barbu_score"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
